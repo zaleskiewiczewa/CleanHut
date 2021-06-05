588116dccdc2cccab2fd590262beec5e6d96f2a9
ewa <zaleskiewicz.ewa@onet.pl>
2021-06-05 21:44:34 +0200

    commit 22

--- src/main/resources/META-INF.sql/data.sql
+++ src/main/resources/META-INF.sql/data.sql
@@ -1,4 +1,5 @@
-INSERT INTO space VALUES (1, 'Bathroom'), (2, 'Living room'), (3, 'Bedroom');
+INSERT INTO space VALUES ( 1,'Bathroom'),(2, 'Living room'), (3, 'Bedroom');
 INSERT INTO data VALUES (1, 'Week 24', 'Bathroom'), (2, 'Week 27', 'Kitchen'), (3. 'Week 28', 'Bedroom');
-INSERT INTO activities VALUES (1, 'Ironing'), (2, 'Clean window'), (3, 'Clean furniture');
-INSERT INTO dates_rooms VALUES (1,'Week 23','Kitchen'), (2,'Week 25', 'Bathroom'), (3, 'Week 28', 'Living room ');
+INSERT INTO activities VALUES (1,'Ironing');(2, 'Clean window'), (3, 'Clean furniture');
+INSERT INTO dates_rooms VALUES (1,'Kitchen'),(2,'Week 25', 'Bathroom'), (3, 'Week 28', 'Living room ');
+INSERT INTO users_activities VALUES (1, 2), (2, 3);
